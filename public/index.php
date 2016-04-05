b53e7f3d4611d2ff61880856c2c3e66ad14b6dd0
Fernando Andre <netriver@gmail.com>
2016-04-05 19:00:04 -0300

    use composer and vendor directory

--- public/index.php
+++ public/index.php
@@ -10,9 +10,12 @@ defined('APPLICATION_ENV')
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
+     realpath(APPLICATION_PATH . '/../library'),
+     realpath(APPLICATION_PATH . '/../vendor/zendframework/zendframework1/library'),
+     get_include_path()
 )));
+
+require_once realpath(APPLICATION_PATH . '/../vendor/autoload.php');
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
